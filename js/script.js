c62aab45f024737fb18e8be81d63911037dd6fc8
Monika Kamecka <monika.kamecka@gmail.com>
2021-01-14 21:28:06 +0100

    Add modified js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
   /* remove class 'active' from all article links usuń klasę active ze wszystkich linków na liście tytułów */
   const activeLinks = document.querySelectorAll('.titles a.acive');
-  for (let aciveLink of activeLinks){
+  for (let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
   /* add class 'active' to the clicked link dodaj klasę active do klikniętego linka*/
-
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
   /* remove class 'active' from all articles usuń klasę active ze wszystkich artykułów*/
-  const activeArticles = document.querySelectorAll('.posts .post.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
   for (let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link z klikniętego linka weź zawartość atrybutu href, np. #article-2*/
-
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) najdź na stronie element pasujący do selektora takiego, jak wartość atrybutu href, np. #article-2 – czyli szukamy elementu o id="article-2"*/
-
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
   /* add class 'active' to the correct article dodaj klasę active do znalezionego artykułu*/
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
