33de3912b05efaa81fa74e0caa2750a0f940935b
Monika Kamecka <monika.kamecka@gmail.com>
2021-01-14 17:36:58 +0100

    Remove active links and active articles

--- js/script.js
+++ js/script.js
@@ -4,17 +4,24 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* remove class 'active' from all article links usuń klasę active ze wszystkich linków na liście tytułów */
+  const activeLinks = document.querySelectorAll('.titles a.acive');
+  for (let aciveLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-  /* add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link dodaj klasę active do klikniętego linka*/
 
-  /* remove class 'active' from all articles */
+  /* remove class 'active' from all articles usuń klasę active ze wszystkich artykułów*/
+  const activeArticles = document.querySelectorAll('.posts .post.active');
+  for (let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+  /* get 'href' attribute from the clicked link z klikniętego linka weź zawartość atrybutu href, np. #article-2*/
 
-  /* get 'href' attribute from the clicked link */
+  /* find the correct article using the selector (value of 'href' attribute) najdź na stronie element pasujący do selektora takiego, jak wartość atrybutu href, np. #article-2 – czyli szukamy elementu o id="article-2"*/
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+  /* add class 'active' to the correct article dodaj klasę active do znalezionego artykułu*/
 }
 
 const links = document.querySelectorAll('.titles a');
